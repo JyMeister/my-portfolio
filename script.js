145e0f5ec71bdde077cb5f4ec4775975625f118f
Jay Braun <jab-96@live.com>
2026-08-07 18:53:41 -0500

    delete dead code

--- script.js
+++ script.js
@@ -17,7 +17,11 @@ const toggleButton = document.getElementById('darkModeToggle');
 
 
 const title = "Welcome to my Portfolio!";
+const message = "Welcome to my portfolio website! I'm Jay Braun, a junior web developer with a passion for creating clean, " +
+ "functional, and creative websites. This site is a showcase of my skills, projects, and journey in the world of web development. " +
+ "Feel free to explore and learn more about me and my work!";
 const typingTitle = document.getElementById("typing-title");
+const typingMessage = document.getElementById("typing-message");
 let i = 0;
 
 function type() {
@@ -30,6 +34,16 @@ function type() {
   }
 }
 type();
+
+let j = 0;
+function typeMessage() {
+  if (j < message.length) {
+    typingMessage.textContent += message[j];
+    j++;
+    setTimeout(typeMessage, 50); // adjust speed here (lower = faster)
+  }
+}
+typeMessage();
 
 
 
