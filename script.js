09c3e236a406897eed78a6c61344eb689a71855c
Jay Braun <jab-96@live.com>
2026-08-07 18:09:34 -0500

    Changes: Index.html > background nav color

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ const toggleButton = document.getElementById('darkModeToggle');
       toggleButton.textContent = '🌙 Dark Mode';
     }
   });
+
+
+
+
+const title = "Welcome to my Portfolio!";
+const typingTitle = document.getElementById("typing-title");
+let i = 0;
+
+function type() {
+  if (i < title.length) {
+    typingTitle.textContent += title[i];
+    i++;
+    setTimeout(type, 150); // adjust speed here (lower = faster)
+  } else {
+    typingTitle.classList.remove("typing-cursor"); // remove cursor when done
+  }
+}
+type();
+
+
+
+
