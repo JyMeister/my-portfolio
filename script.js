e39ebbee0efbad53b373227e7862339ed6d4dbf6
Jay Braun <jab-96@live.com>
2026-08-08 12:13:25 -0500

    checkpoint: changed welcome message, speed, and layout

--- script.js
+++ script.js
@@ -17,9 +17,7 @@ const toggleButton = document.getElementById('darkModeToggle');
 
 
 const title = "Welcome to my Portfolio";
-const message = "Welcome to my portfolio website! I'm Jay Braun, a junior web developer with a passion for creating clean, " +
- "functional, and creative websites. This site is a showcase of my skills, projects, and journey in the world of web development. " +
- "Feel free to explore and learn more about me and my work!";
+const message = "Hi! My name is Jay Braun. I am a Junior Web Developer.";
 const typingTitle = document.getElementById("typing-title");
 const typingMessage = document.getElementById("typing-message");
 let i = 0;
@@ -40,7 +38,7 @@ function typeMessage() {
   if (j < message.length) {
     typingMessage.textContent += message[j];
     j++;
-    setTimeout(typeMessage, 11); // adjust speed here (lower = faster)
+    setTimeout(typeMessage, 60); // adjust speed here (lower = faster)
   }
 }
 typeMessage();
